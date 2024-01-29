3236b1e198b1dc9eb15395d1c9ead073cbd8e3dc
Daniil Chesnokov <deerektor@gmail.com>
2024-01-29 22:16:03 +0300

    Implementing data layer

--- app/src/main/java/com/slyene/calories/data/DishDao.kt
+++ app/src/main/java/com/slyene/calories/data/DishDao.kt
@@ -1,4 +1,26 @@
 package com.slyene.calories.data
 
-class DishDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DishDao {
+    @Insert
+    suspend fun insert(item: Dish)
+
+    @Update
+    suspend fun update(item: Dish)
+
+    @Delete
+    suspend fun delete(item: Dish)
+
+    @Query("SELECT * FROM meals")
+    fun getDishes(): Flow<List<Dish>>
+
+    @Query("SELECT * FROM meals WHERE id = :id")
+    fun getDishById(id: Int): Flow<Dish>
 }
--- app/src/main/java/com/slyene/calories/data/MealDao.kt
+++ app/src/main/java/com/slyene/calories/data/MealDao.kt
@@ -1,4 +1,26 @@
 package com.slyene.calories.data
 
-class MealDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MealDao {
+    @Insert
+    suspend fun insert(item: Meal)
+
+    @Update
+    suspend fun update(item: Meal)
+
+    @Delete
+    suspend fun delete(item: Meal)
+
+    @Query("SELECT * FROM meals")
+    fun getMeals(): Flow<List<Meal>>
+
+    @Query("SELECT * FROM meals WHERE id = :id")
+    fun getMealById(id: Int): Flow<Meal>
 }
